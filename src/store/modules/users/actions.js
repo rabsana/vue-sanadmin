25238f8342aea1353e623cecb92898aa18c47bcf
Totalexcoin <totalexcoin@gmail.com>
2019-03-21 23:29:12 +0330

    Bug fixed in fetch users action

--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -6,7 +6,7 @@ import commonActionTypes from '../common/action-types'
 export default {
   [actionTypes.FETCH_USERS_LIST]: function ({ commit, dispatch }, payload) {
     commit(mutationTypes.USERS_LIST_REQUEST)
-    let method = 'post'
+    let method = 'get'
     let uri = uris.USERS_PATH
     dispatch('sanadmin/common/' + commonActionTypes.API_CALL, { method, uri }, { root: true }).then(function (result) {
       commit(mutationTypes.USERS_LIST_SUCCESS, result.data)
--- src/store/modules/users/mutations.js
+++ src/store/modules/users/mutations.js
@@ -1,14 +1,14 @@
 import mutationTypes from './mutation-types'
 
 export default {
-  [mutationTypes.USERS_LIST_REQUEST]: function (state) {
+  [mutationTypes.USERS_LIST_REQUEST]: function (state, payload) {
     state.usersListLoading = true
   },
   [mutationTypes.USERS_LIST_SUCCESS]: function (state, payload) {
     state.usersListLoading = false
     state.usersList = payload
   },
-  [mutationTypes.USERS_LIST_FAILURE]: function (state) {
+  [mutationTypes.USERS_LIST_FAILURE]: function (state, payload) {
     state.usersListLoading = false
   }
 }
