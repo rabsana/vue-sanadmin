e07db7ab0de6bcf7261f3178e5a3a92b71d1ac13
Totalexcoin <totalexcoin@gmail.com>
2019-03-21 12:33:15 +0330

    Bug fixed in module registering namespace

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import commonActionTypes from './store/modules/common/action-types'
 export default {
   install (Vue, options) {
     let { baseUrl, apiKey, store } = options
-    store.registerModule('admin/common', commonModule)
-    store.registerModule('admin/auth', authModule)
+    store.registerModule('sanadmin/common', commonModule)
+    store.registerModule('sanadmin/auth', authModule)
     store.dispatch('sanadmin/common/'+commonActionTypes.SET_BASE_URL, baseUrl)
     store.dispatch('sanadmin/common/'+commonActionTypes.SET_API_KEY, apiKey)
   }
