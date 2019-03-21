c5bfd42a64440ad15c3640822d553059d88439d8
Totalexcoin <totalexcoin@gmail.com>
2019-03-21 23:57:11 +0330

    getters created for better accessing in usersList data

--- src/store/modules/users/getters.js
+++ src/store/modules/users/getters.js
@@ -1,3 +1,16 @@
 export default {
-
+  usersArray: (state, getters) => {
+    try {
+      return state.usersList.data
+    } catch (e) {
+      return []
+    }
+  },
+  usersPagination: (state, getters) => {
+    try {
+      return state.usersList.meta.pagination
+    } catch (e) {
+      return {}
+    }
+  }
 }
